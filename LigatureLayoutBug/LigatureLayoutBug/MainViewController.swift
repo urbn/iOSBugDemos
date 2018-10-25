f99768e9fdc2a2ccf9c2afe31ef48a22f7c3fe36
Nicole Souvenir <nsouvenir@urbn.com>
2018-10-25 10:36:50 -0400

    added collection view to mainViewController and set navigation controller title

--- LigatureLayoutBug/LigatureLayoutBug/MainViewController.swift
+++ LigatureLayoutBug/LigatureLayoutBug/MainViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let buttonView = LabelCollectionView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Button Flow Layout Bug"
+        view.backgroundColor = .white
+        view.addSubview(buttonView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
