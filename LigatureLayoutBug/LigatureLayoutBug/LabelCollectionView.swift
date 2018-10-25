17b5781be1e08910ac8c574f5ff024661dee62c8
Nicole Souvenir <nsouvenir@urbn.com>
2018-10-25 10:32:54 -0400

    initialized, constrained and set layout for collection view with a datasource

---
+++ LigatureLayoutBug/LigatureLayoutBug/LabelCollectionView.swift
@@ -0,0 +1,43 @@
+//
+//  LabelCollectionView.swift
+//  LigatureLayoutBug
+//
+//  Created by Nicole Souvenir on 10/25/18.
+//  Copyright © 2018 Nicole Souvenir. All rights reserved.
+//
+
+import UIKit
+
+final class LabelCollectionView: UIView {
+
+    private let collectionView: UICollectionView
+    private var dataSource = ["Red", "Orange", "Yellow", "Green"]
+    
+    init() {
+        let spaceFromTop: CGFloat = 10
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 0.0
+        flowLayout.itemSize = CGSize(width: (UIScreen.main.bounds.width/2 - spaceFromTop), height: 100)
+        flowLayout.minimumInteritemSpacing = 12
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        flowLayout.scrollDirection = .horizontal
+        
+        self.collectionView = UICollectionView(frame: UIScreen.main.bounds, collectionViewLayout: flowLayout)
+        super.init(frame: .zero)
+        
+        collectionView.collectionViewLayout = flowLayout
+        collectionView.heightAnchor.constraint(equalToConstant: 40.0)
+        collectionView.dataSource = self
+        collectionView.register(LabelCollectionViewCell.self, forCellWithReuseIdentifier: "ButtonCell")
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        addSubview(collectionView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implement ed")
+    }
+}
+
+
+}
