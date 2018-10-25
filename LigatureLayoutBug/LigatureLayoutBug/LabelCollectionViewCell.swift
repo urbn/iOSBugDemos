43d64402506cd496f1cb23e3adce92f89b1fb2c4
Nicole Souvenir <nsouvenir@urbn.com>
2018-10-25 10:24:32 -0400

    create a collectionview cell that initializes and constrains a label

---
+++ LigatureLayoutBug/LigatureLayoutBug/LabelCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  LabelCollectionViewCell.swift
+//  LigatureLayoutBug
+//
+//  Created by Nicole Souvenir on 10/25/18.
+//  Copyright © 2018 Nicole Souvenir. All rights reserved.
+//
+
+import UIKit
+
+final class LabelCollectionViewCell: UICollectionViewCell {
+    
+    var label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.backgroundColor = .green
+        label.textColor = .black
+        label.textAlignment = .center
+        
+        contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
