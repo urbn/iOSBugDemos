c2a77a09af15255d321500827adfa6bb8a8b1017
Nicole Souvenir <nsouvenir@urbn.com>
2018-10-25 10:19:39 -0400

    deleted storyboard and added window with NavigationController  in AppDelegate

---
+++ LigatureLayoutBug/LigatureLayoutBug/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LigatureLayoutBug
+//
+//  Created by Nicole Souvenir on 10/25/18.
+//  Copyright © 2018 Nicole Souvenir. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
